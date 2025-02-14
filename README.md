05308e391c890ea7aff6bd8f79a30b162fe6f715
Reid Connor Moseley <rmosele2@gal-i9.charlotte.edu>
2025-02-14 12:20:55 -0500

    uploading file

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # mergeSort
-merge sort algorithm 
+merge sort algorithm
+
+# Merge Sort Implementation
+
+## Description
+
+This C++ program implements the Merge Sort algorithm to sort an array of integers. It generates random data for the array and measures the execution time of the sorting process.
+
+## Compilation
+
+To compile the program, use the following command:
+```shell
+make
+
---
+++ mergeSort.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <cstdlib>
+
+void merge(std::vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    std::vector<int> L(n1);
+    std::vector<int> R(n2);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0;
+    int j = 0;
+    int k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(std::vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <array_size>" << std::endl;
+        return 1;
+    }
+
+    int size = std::atoi(argv[1]);
+    if (size <= 0) {
+         std::cerr << "Array size must be positive" << std::endl;
+        return 1;
+    }
+
+    std::vector<int> data(size);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(1, 1000);
+
+    for (int i = 0; i < size; i++) {
+        data[i] = distrib(gen);
+    }
+
+    auto start = std::chrono::high_resolution_clock::now();
+    mergeSort(data, 0, size - 1);
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+    std::cerr << "Time taken by Merge Sort: " << duration.count() << " microseconds" << std::endl;
+    
+    return 0;
+}
